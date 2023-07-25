de599c4a9edf8c260d3e90a085704576e9658181
Yaron Shamir <temp@yaronshamir.com>
2023-07-25 17:37:41 +1000

    Added build files to src

---
+++ src/build.js
@@ -0,0 +1,23 @@
+const componentsDir = process.argv[2];
+
+
+require('./components').build(componentsDir);
+require('./templates').build(componentsDir);
+const style = require('./style');
+style.build(componentsDir);
+
+
+if (process.argv.length > 3) {
+    const layoutDir = process.argv[3];
+    require('./layouts').build(layoutDir);
+    style.build(layoutDir);
+}
+
+/*
+// const layouts = require('./layouts');
+// layouts.build();
+
+const sass =;
+sass;
+
+*/
